6f902990f244d8cf0b72d667181f83533bd5f756
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-07-20 18:24:19 +0530

    added routes boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      namespace :management do
+        get 'visitor/create'
+        get 'visitor/read'
+        get 'visitor/update'
+        get 'visitor/delete'
+
+        get 'transaction/create'
+        get 'transaction/read'
+        get 'transaction/update'
+        get 'transaction/delete'
+
+        get 'admission/create'
+        get 'admission/read'
+        get 'admission/update'
+        get 'admission/delete'
+	  end
+	  namespace :setup do
+        post 'building/create' => 'building#create'
+      end
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
