1f81574a79fb764228ca09d024b932a167075bad
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-07-22 22:27:07 +0530

    enabled session, cookies middleware

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module IPM
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_ipm'
   end
 end
