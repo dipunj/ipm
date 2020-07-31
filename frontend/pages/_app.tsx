f9c348ba434978bce062945a1c4a7136341d1640
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-01 02:01:44 +0530

    added tailwind with postcss, not adding purgecss, as that is inbuilt now.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import whyDidYouRender from '@welldone-software/why-did-you-render';
 
+import '../styles/index.css';
+
 if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
 	whyDidYouRender(React, { trackAllPureComponents: true });
 }
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	purge: ['./components/**/*.tsx', './component-library/**/*.tsx'],
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
