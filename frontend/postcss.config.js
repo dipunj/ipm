d34d98f4b60db0a98b0da231bcf71e4e367c0b9f
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-01 14:18:20 +0530

    Added default nextjs config

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,3 +1,24 @@
+// nextjs also uses postcss to transform css,
+// do not provide plugins in the require() function
+// as stated in tailwindcss docs, since nextjs would break
+// if plugins are provided
+
+// so just use the default postcss config for nextjs and add tailwindcss plugin to it
 module.exports = {
-	plugins: ['tailwindcss', 'postcss-preset-env'],
+	plugins: [
+		'tailwindcss',
+		'postcss-flexbugs-fixes',
+		[
+			'postcss-preset-env',
+			{
+				autoprefixer: {
+					flexbox: 'no-2009',
+				},
+				stage: 3,
+				features: {
+					'custom-properties': false,
+				},
+			},
+		],
+	],
 };
