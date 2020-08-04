3ba01a9a5cdc0903524d50b04f6e428e68e27a8a
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-04 19:09:35 +0530

    removed all ui stuff

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,16 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/,
-			issuer: {
-				test: /\.(js|ts)x?$/,
-			},
-			use: ['@svgr/webpack'],
-		});
-
-		return config;
-	},
-});
+module.exports = withBundleAnalyzer({});
