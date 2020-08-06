1048e447231ae29206b1966d73ef258f1156e820
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-06 20:36:23 +0530

    added landing page with styles, added request wrapper

--- frontend/components/Landing/index.tsx
+++ frontend/components/Landing/index.tsx
@@ -1,33 +1,98 @@
 // import { useState } from 'react';
 
 import { useState } from 'react';
-import Modal from '../../library/Modal';
+import { Button, Dialog, FormGroup, InputGroup } from '@blueprintjs/core';
+import { Background, Column, Logo, CenterColumn, Brand, AppName } from './styles';
+import request from '../../library/Request';
+
+const signInButtonStyles = {
+	background: '#EA3333',
+	fontSize: '24px',
+	padding: '16px 32px',
+	borderRadius: 0,
+	fontWeight: 300,
+};
+
+const dialogStyles = { width: 'min(300px, 100vw)' };
 
 const Landing = (): JSX.Element => {
-	const [show, setShow] = useState(false);
+	const [open, setOpen] = useState(false);
 
-	const toggleModal = () => {
-		setShow((show) => !show);
+	const [loginId, setLoginId] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleLoginChange = ({ target: { value } }) => setLoginId(value);
+	const handlePasswordChange = ({ target: { value } }) => setPassword(value);
+
+	const toggleModal = () => setOpen((open) => !open);
+
+	const handleSubmit = async () => {
+		try {
+			const response = await request.post('/session/auth/login', {
+				login_id: loginId,
+				password,
+			});
+			alert(JSON.stringify(response));
+		} catch (error) {
+			alert(error);
+		}
 	};
 
-	const modalContent = (
-		<div>
-			<input type="text" name="loginid" placeholder="login id" />
-			<input type="password" name="password" placeholder="password" />
-		</div>
-	);
-
 	return (
-		<div>
-			<div>
-				<img src="/images/logo_large.png" alt="hospital logo" />
-			</div>
-			<div>
-				<h1>{process.env.NEXT_PUBLIC_HOSPITAL_NAME}</h1>
-				<h2>In-Patient Department</h2>
-				<button>Sign-In</button>
-			</div>
-		</div>
+		<Background>
+			<CenterColumn>
+				<Logo src="/images/logo_large.png" alt="Hospital Logo" />
+			</CenterColumn>
+			<Column>
+				<Brand>{process.env.NEXT_PUBLIC_HOSPITAL_NAME}</Brand>
+				<AppName>In-Patient Department</AppName>
+				<div>
+					<Button
+						intent="danger"
+						text="Sign In"
+						large
+						style={signInButtonStyles}
+						onClick={toggleModal}
+					/>
+				</div>
+			</Column>
+			<Dialog title="Login" isOpen={open} onClose={toggleModal} style={dialogStyles}>
+				<form>
+					<div className="bp3-dialog-body">
+						<FormGroup labelFor="loginid" labelInfo="(required)">
+							<InputGroup
+								id="loginid"
+								name="login_id"
+								placeholder="login id"
+								large
+								value={loginId}
+								onChange={handleLoginChange}
+							/>
+						</FormGroup>
+						<FormGroup labelFor="password" labelInfo="(required)">
+							<InputGroup
+								large
+								id="password"
+								type="password"
+								name="password"
+								placeholder="password"
+								value={password}
+								onChange={handlePasswordChange}
+							/>
+						</FormGroup>
+					</div>
+					<div className="bp3-dialog-footer">
+						<Button
+							type="submit"
+							intent="primary"
+							text="Sign In"
+							onClick={handleSubmit}
+							fill
+						/>
+					</div>
+				</form>
+			</Dialog>
+		</Background>
 	);
 };
 
---
+++ frontend/components/Landing/styles.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const Background = styled.div`
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: row;
+	background: #1c1c1c;
+`;
+
+export const Column = styled.div`
+	display: flex;
+	width: 50%;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+export const CenterColumn = styled(Column)`
+	align-items: center;
+`;
+
+export const Logo = styled.img`
+	width: 300px;
+	height: auto;
+`;
+
+export const Brand = styled.h1`
+	font-family: Amiko;
+	font-weight: 100;
+	font-size: 58px;
+	color: white;
+`;
+
+export const AppName = styled.h2`
+	font-family: 'Exo 2';
+	font-size: 36px;
+	font-weight: lighter;
+	color: white;
+`;
