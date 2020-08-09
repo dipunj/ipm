bff50618a18d39ee87f3c81cd7251cd064f93663
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-10 04:13:03 +0530

    header almost complete, menu next

--- frontend/library/Layout/Header/index.tsx
+++ frontend/library/Layout/Header/index.tsx
@@ -1,24 +1,51 @@
 import { useContext, useState } from 'react';
+import { Button, Switch, Icon } from '@blueprintjs/core';
+import { useRouter } from 'next/router';
 import { SessionCtx } from '../../Context/SessionContext';
 import { prettyJSON } from '../../../helpers';
 import { ThemeCtx } from '../../Context/ThemeContext';
-import { Container } from './styles';
+import { Container, Greeting, Wrapper, Column } from './styles';
+import request from '../../Request';
+
+const switchStyle = { marginBottom: 0, marginRight: '16px' };
 
 const Header = (): JSX.Element => {
 	const {
-		ctx: { name, account_type },
+		ctx: { name },
+		setCtx,
 	} = useContext(SessionCtx);
-	const { isDark, setIsDark, toggleTheme } = useContext(ThemeCtx);
+
+	const router = useRouter();
+	const { isDark, toggleTheme } = useContext(ThemeCtx);
 	const handleLogout = async () => {
 		const response = await request.post('/session/auth/logout');
-		// TODO show toast here depending on response
+		if (response.data.success) {
+			setCtx({});
+			router.push('/login');
+		}
 	};
 
 	return (
-		<Container>
-			Hi, {name}
-			<button onClick={toggleTheme}>Toggle</button>
-		</Container>
+		<Wrapper isDark={isDark}>
+			<Container>
+				<Column>
+					<Icon icon="menu" />
+					<Greeting>Hi, {name}</Greeting>
+				</Column>
+				<Column>
+					<Switch
+						checked={isDark}
+						onClick={toggleTheme}
+						innerLabel="light"
+						innerLabelChecked="dark"
+						style={switchStyle}
+					/>
+					<Button intent="danger" onClick={handleLogout}>
+						Sign Out
+					</Button>
+				</Column>
+			</Container>
+		</Wrapper>
 	);
 };
 
--- frontend/library/Layout/Header/styles.tsx
+++ frontend/library/Layout/Header/styles.tsx
@@ -1,9 +1,34 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Wrapper = styled.div`
 	/* padding: 0px var(--page-padding); */
-	height: ${(props) => (props.height ? props.height : 'var(--header-height)')};
-	/* background: inherit; */
+	min-height: ${(props) => (props.height ? props.height : 'var(--header-height)')};
+	box-shadow: ${(props) =>
+		props.isDark
+			? '0px 4px 4px 0px var(--dark-shadow)'
+			: '0px 4px 4px 0px var(--light-shadow)'};
+	width: 100%;
+	background: inherit;
+
+	position: fixed;
+	top: 0;
 `;
 
-export { Container };
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
+	height: 100%;
+	padding: 28px 48px;
+`;
+
+const Column = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Greeting = styled.div`
+	margin-left: 40px;
+`;
+export { Wrapper, Container, Column, Greeting };
