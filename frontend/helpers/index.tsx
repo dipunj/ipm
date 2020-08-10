43166a9b05040dc3e1867458a89b1f00b27fe068
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-10 17:49:53 +0530

    added ls helpers

--- frontend/helpers/index.tsx
+++ frontend/helpers/index.tsx
@@ -1,1 +1,25 @@
 export const prettyJSON = (data) => <pre>{JSON.stringify(data, null, '\t')}</pre>;
+
+export const getFromLS = (key) => {
+	const state = JSON.parse(localStorage.getItem('data'));
+	if (state) {
+		if (state.hasOwnProperty(key)) return state[key];
+	} else {
+		return null;
+	}
+};
+
+export const saveToLS = (key, value) => {
+	let state = JSON.parse(localStorage.getItem('data'));
+	if (state) {
+		state = {
+			...state,
+			[key]: value,
+		};
+	} else {
+		state = {
+			[key]: value,
+		};
+	}
+	localStorage.setItem('data', JSON.stringify(state));
+};
