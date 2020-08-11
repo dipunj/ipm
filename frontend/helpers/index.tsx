2be51d8912c800376f51c32b0aa4cfe02b4a171e
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-11 16:09:08 +0530

    added cookie helper

--- frontend/helpers/index.tsx
+++ frontend/helpers/index.tsx
@@ -23,3 +23,31 @@ export const saveToLS = (key, value) => {
 	}
 	localStorage.setItem('data', JSON.stringify(state));
 };
+
+export const getCookie = (key) => {
+	const jsCookies = document.cookie;
+	if (jsCookies) {
+		const parsedCookies = jsCookies.split('; ').reduce((acc, kv) => {
+			const [k, v] = kv.split(/=(.+)/);
+			return {
+				...acc,
+				[k]: v,
+			};
+		}, {});
+		return parsedCookies[key];
+	}
+	return null;
+};
+
+export const setCookie = (key, value) => {
+	// const jsCookies = document.cookie;
+	// const parsedCookies = jsCookies.split('; ').reduce((acc, kv) => {
+	// 	const [k, v] = kv.split(/=(.+)/);
+	// 	return {
+	// 		...acc,
+	// 		[k]: v,
+	// 	};
+	// }, {});
+	// parsedCookies[key] = value;
+	document.cookie = `${key}=${value}`;
+};
