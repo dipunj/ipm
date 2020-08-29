c3cdf1a1bfec4f2e4d67f80e4da11a6ca9c02ad7
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-30 01:00:19 +0530

    ssred transactions page

---
+++ frontend/pages/transactions/[admission_id]/index.tsx
@@ -0,0 +1,35 @@
+import { GetServerSideProps } from 'next';
+import request from '../../../library/Request';
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+// 	const {
+// 		params: { admission_id },
+// 	} = context;
+
+// 	return {
+// 		props: {
+// 			admission_id,
+// 		},
+// 	};
+// };
+
+// TODO: CHECK why are cookies not being sent to backend on ssr
+// because we need to explicitly tell axios to send them in header
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const {
+		params: { admission_id },
+	} = context;
+
+	const res = await request.get('/management/transaction/list', {
+		params: {
+			admission_id,
+		},
+		headers: context.req ? { cookie: context.req.headers.cookie } : undefined,
+	});
+	console.log(res.data.response);
+	return {
+		props: { transactionsAPIResponse: res.data },
+	};
+};
+
+export { default } from '../../../components/Transaction';
