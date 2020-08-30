762b14ce5fe0545170813ac5ef85232bc0d6d0d8
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-30 17:40:10 +0530

    added ssg logic for details admission page

--- frontend/pages/admission/[admission_id]/index.tsx
+++ frontend/pages/admission/[admission_id]/index.tsx
@@ -1,35 +1,45 @@
-import { GetServerSideProps } from 'next';
-import request from '../../../library/Request';
+import { GetStaticProps } from 'next';
 
 export { default } from '../../../components/Admission/ViewExisting';
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
+// export const getStaticProps: getStaticProps = async (context) => {
 // 	const {
 // 		params: { admission_id },
 // 	} = context;
 
-// 	return {
-// 		props: {
+// 	const res = await request.get('/management/admission/find', {
+// 		params: {
 // 			admission_id,
 // 		},
+// 		headers: context.req ? { cookie: context.req.headers.cookie } : undefined,
+// 	});
+// 	return {
+// 		props: { admssionAPIResponse: res },
 // 	};
 // };
 
 // TODO: CHECK why are cookies not being sent to backend on ssr
 // because we need to explicitly tell axios to send them in header
-export const getServerSideProps: GetServerSideProps = async (context) => {
+
+export const getStaticPaths = async () => {
+	return {
+		paths: ['/admission/[admission_id]'],
+		fallback: true,
+	};
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
 	const {
 		params: { admission_id },
 	} = context;
 
-	const res = await request.get('/management/admission/find', {
-		params: {
-			admission_id,
-		},
-		headers: context.req ? { cookie: context.req.headers.cookie } : undefined,
-	});
-	console.log(res.data.response);
+	// const res = await request.get('/management/admission/find', {
+	// 	params: {
+	// 		admission_id,
+	// 	},
+	// 	headers: context.req ? { cookie: context.req.headers.cookie } : undefined,
+	// });
 	return {
-		props: { admissionAPIResponse: res.data },
+		props: { admission_id },
 	};
 };
