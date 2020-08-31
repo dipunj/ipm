ef7c46667e25d2b3fccd4cf7902c006e68554719
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-08-31 19:58:40 +0530

    integrated with new api for transactions

--- frontend/components/Transaction/index.tsx
+++ frontend/components/Transaction/index.tsx
@@ -1,18 +1,19 @@
 import { Table, Column } from '@blueprintjs/table';
 import { Button } from '@blueprintjs/core';
-import { TableContainer, TableWrapper } from './styles';
+import { TableContainer, TableWrapper, TableLayout } from './styles';
 import NewTransaction from './NewTransaction';
 import useFetch from '../../library/hooks/fetch';
 import useLayout from './Layout';
 import Providers from '../../library/Providers';
 import Header from '../../library/Layout/Header';
+import TotalOverview from './TotalOverview';
 
 const Transactions = ({ admission_id }: { admission_id: string }): JSX.Element => {
-	const { loading, data: list, refetch } = useFetch('/management/transaction/list', {
+	const { loading, data, refetch } = useFetch('/management/transaction/list', {
 		params: { admission_id },
 	});
 
-	const { columns } = useLayout(list);
+	const { columns } = useLayout(data?.list, refetch);
 
 	if (loading) {
 		return <div>loading...</div>;
@@ -20,16 +21,16 @@ const Transactions = ({ admission_id }: { admission_id: string }): JSX.Element =
 
 	return (
 		<TableWrapper>
-			<Button icon="arrow-left" intent="warning" minimal>
-				Back
-			</Button>
-			<TableContainer>
-				<Table numRows={list.length}>
-					{columns.map((col) => (
-						<Column key={col.name} name={col.name} cellRenderer={col.cellRender} />
-					))}
-				</Table>
-			</TableContainer>
+			<TableLayout>
+				<TableContainer>
+					<Table numRows={data.list.length}>
+						{columns.map((col) => (
+							<Column key={col.name} name={col.name} cellRenderer={col.cellRender} />
+						))}
+					</Table>
+				</TableContainer>
+				<TotalOverview totals={data.totals} admission_info={data.admission_info} />
+			</TableLayout>
 			<NewTransaction refetch={refetch} />
 		</TableWrapper>
 	);
