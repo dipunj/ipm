16f4e25989674c47124eb6bd7465516448f58a1b
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-09-04 02:03:02 +0530

    integrated password change api
    Added conditionwise markers to show which password validation condition is not satisfied

--- frontend/components/Profile/Password/index.tsx
+++ frontend/components/Profile/Password/index.tsx
@@ -1,9 +1,14 @@
 import { useContext, useState, ChangeEvent } from 'react';
-import { FormGroup, InputGroup, Button } from '@blueprintjs/core';
+import { FormGroup, InputGroup, Button, Callout, Colors, Popover } from '@blueprintjs/core';
 import { SessionCtx } from '../../../library/Context/SessionContext';
-import { InputContainer, ButtonContainer } from './styles';
+import { InputContainer, ButtonContainer, ListItem, ChecksContainer } from './styles';
+import { validatePassword } from '../../../helpers';
+import { handleErrorToast, handleSuccessToast } from '../../../library/Toaster';
+import request from '../../../library/Request';
 
 const ResetPassword = (): JSX.Element => {
+	const { refetchCtx }: any = useContext(SessionCtx);
+
 	const [params, setParams] = useState({
 		password: '',
 		passwordConfirmation: '',
@@ -20,50 +25,92 @@ const ResetPassword = (): JSX.Element => {
 		}));
 	};
 
-	const allOkay = params.password === params.passwordConfirmation && params.password !== '';
+	const passwordChecks = validatePassword(params.password);
+	const isPasswordValid = passwordChecks.filter((chck) => !chck.satisfy).length === 0;
+
+	const handlePasswordChange = async () => {
+		try {
+			const response = await request.post('/session/auth/reset', {
+				password: params.password,
+				password_confirmation: params.passwordConfirmation,
+			});
+			if (response.data.success) {
+				handleSuccessToast(response);
+				refetchCtx();
+			}
+		} catch (error) {
+			handleErrorToast(error);
+		}
+	};
+
+	const passwordConfirmMatch =
+		params.password === params.passwordConfirmation && params.password !== '';
+
+	const buttonIntent = passwordConfirmMatch
+		? isPasswordValid
+			? 'success'
+			: 'warning'
+		: 'danger';
+
 	return (
 		<div className="page-content">
 			<h1 className="page-title">Reset Password</h1>
 			<div className="column center full-width">
-				<InputContainer>
-					<FormGroup
-						className="full-width"
-						label="New Password"
-						labelFor="password"
-						labelInfo="(required)"
-					>
-						<InputGroup
-							fill
-							value={params.password}
-							onChange={handleInputChange}
-							id="password"
-							name="password"
-							placeholder="New Password"
-							type="password"
-						/>
-					</FormGroup>
-					<FormGroup
-						className="full-width"
-						label="Confirm Password"
-						labelFor="password-confirmation"
-						labelInfo="(required)"
-					>
-						<InputGroup
-							fill
-							value={params.passwordConfirmation}
-							onChange={handleInputChange}
-							id="password-confirmation"
-							name="passwordConfirmation"
-							placeholder="New Password"
-							type="password"
-						/>
-					</FormGroup>
-				</InputContainer>
-				<ButtonContainer>
-					<Button disabled={!allOkay} intent={allOkay ? 'success' : 'danger'} large fill>
-						Submit
-					</Button>
-				</ButtonContainer>
+				<div className="wrap row space-evenly align-center full-width">
+					<InputContainer>
+						<FormGroup
+							className="full-width"
+							label="New Password"
+							labelFor="password"
+							labelInfo="(required)"
+						>
+							<InputGroup
+								fill
+								value={params.password}
+								onChange={handleInputChange}
+								id="password"
+								name="password"
+								placeholder="New Password"
+								type="password"
+							/>
+						</FormGroup>
+						<FormGroup
+							className="full-width"
+							label="Confirm Password"
+							labelFor="password-confirmation"
+							labelInfo="(required)"
+						>
+							<InputGroup
+								fill
+								value={params.passwordConfirmation}
+								onChange={handleInputChange}
+								id="password-confirmation"
+								name="passwordConfirmation"
+								placeholder="Confirm New Password"
+								type="password"
+							/>
+						</FormGroup>
+						<ButtonContainer>
+							<Button
+								disabled={!passwordConfirmMatch || !isPasswordValid}
+								intent={buttonIntent}
+								large
+								fill
+								onClick={handlePasswordChange}
+							>
+								Submit
+							</Button>
+						</ButtonContainer>
+					</InputContainer>
+					<ChecksContainer>
+						Password must have:
+						<ul className="bp3-list">
+							{passwordChecks.map(({ message, satisfy }) => (
+								<ListItem satisfy={satisfy}>{message}</ListItem>
+							))}
+						</ul>
+					</ChecksContainer>
+				</div>
 			</div>
 		</div>
 	);
--- frontend/components/Profile/Password/styles.tsx
+++ frontend/components/Profile/Password/styles.tsx
@@ -1,19 +1,31 @@
 import styled from 'styled-components';
+import { Colors } from '@blueprintjs/core';
 
 export const ButtonContainer = styled.div.attrs({
 	className: 'row full-width full-height',
 })`
 	max-width: 300px;
-	margin: 32px;
+	margin-top: 32px;
 `;
 
 export const InputContainer = styled.div.attrs({
-	className: 'column full-width full-height',
+	className: 'column center full-width full-height',
 })`
 	justify-content: space-between;
 	max-width: 560px;
-	min-height: 276px;
+	/* min-height: 400px; */
 	padding: 64px;
 	border-radius: 36px;
-	background: #0000000c;
+	background: var(--background-highlight);
+`;
+
+export const ListItem = styled.li`
+	color: ${(props: { satisfy: boolean }) => (props.satisfy ? Colors.GREEN5 : Colors.RED5)};
+	margin: 0px !important;
+`;
+
+export const ChecksContainer = styled.div`
+	border-radius: 36px;
+	background: var(--background-highlight);
+	padding: 48px;
 `;
