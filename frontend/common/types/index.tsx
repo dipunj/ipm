8ecd584dea317e73c4ac4d90797f78cc0677252e
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-09-16 01:43:17 +0530

    added ward type type

--- frontend/common/types/index.tsx
+++ frontend/common/types/index.tsx
@@ -1,2 +1,3 @@
 export type Gender = 'male' | 'female' | 'other';
 export type AccountType = 'admin' | 'manager' | 'operator';
+export type WardType = 'General' | 'Luxury' | 'Private' | 'Semi-Private';
