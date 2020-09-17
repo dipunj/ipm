86b6f9e1993f7f67aa4139d7458b4cb8526d872d
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-09-18 04:42:47 +0530

    integrated user form into new user form

--- frontend/components/Organisation/User/New/index.tsx
+++ frontend/components/Organisation/User/New/index.tsx
@@ -1,19 +1,7 @@
-import { useState, SyntheticEvent, ChangeEvent } from 'react';
-import { Button, Callout, FormGroup, InputGroup } from '@blueprintjs/core';
-import AccountTypeSelect from './Select/AccountType';
-import { AccountType } from '../../../../common/types';
-import BuildingSelect, { IBuilding } from '../../../../library/Select/Building';
+import { useState } from 'react';
 import request from '../../../../library/Request';
 import { handleErrorToast, handleSuccessToast } from '../../../../library/Toaster';
-import { Wrapper } from './styles';
-
-interface FormDataType {
-	name: string;
-	login_id: string;
-	password: string;
-	account_type: AccountType;
-	buildings: IBuilding[];
-}
+import UserForm, { UserFormDataType } from './Form';
 
 const initialData = {
 	name: '',
@@ -24,49 +12,7 @@ const initialData = {
 };
 
 const NewUser = () => {
-	const [data, setData]: [FormDataType, any] = useState(initialData);
-
-	const onChange = (fieldName?: string): any => {
-		switch (fieldName) {
-			case 'account_type':
-				return (item: AccountType, _event?: SyntheticEvent<HTMLElement, Event>) => {
-					setData((prev: FormDataType) => ({
-						...prev,
-						[fieldName]: item,
-						buildings: item === 'admin' ? [] : prev.buildings,
-					}));
-				};
-			case 'buildings':
-				return (building: IBuilding) => {
-					setData((prev: FormDataType) => {
-						if (prev.buildings.filter((bld) => bld.id === building.id).length === 1) {
-							// building is already selected
-							const newList = prev.buildings.filter((bld) => bld.id !== building.id);
-							return {
-								...prev,
-								[fieldName]: newList,
-							};
-						}
-						// building is not selected
-						return {
-							...prev,
-							[fieldName]: [...prev.buildings, building],
-						};
-					});
-				};
-			default:
-				return (event: ChangeEvent<HTMLInputElement>) => {
-					const {
-						target: { name, value },
-					} = event;
-
-					setData((prev: FormDataType) => ({
-						...prev,
-						[name]: value,
-					}));
-				};
-		}
-	};
+	const [data, setData]: [UserFormDataType, any] = useState(initialData);
 
 	const handleTagRemove = (_tag: string, index: number) => {
 		setData((prev: FormDataType) => {
@@ -97,90 +43,17 @@ const NewUser = () => {
 			handleErrorToast(error);
 		}
 	};
+
 	return (
-		<div className="page-content">
-			<h1 className="page-title">New User</h1>
-			<Wrapper>
-				<div className="row full-width">
-					<FormGroup label="Full Name" className="full-width">
-						<InputGroup
-							fill
-							name="name"
-							type="text"
-							placeholder="Display Name"
-							value={data.name}
-							onChange={onChange()}
-						/>
-					</FormGroup>
-				</div>
-				<div className="row full-width wrap">
-					<div className="min-half-width">
-						<FormGroup label="Username (Login ID)" className="mr-input">
-							<InputGroup
-								type="text"
-								name="login_id"
-								placeholder="Phone Number / Email ID"
-								value={data.login_id}
-								onChange={onChange()}
-							/>
-						</FormGroup>
-					</div>
-					<div className="min-half-width">
-						<FormGroup
-							label="Password"
-							helperText="Can be changed later by user"
-							// className="min-half-width"
-						>
-							<InputGroup
-								name="password"
-								type="text"
-								placeholder="Password"
-								value={data.password}
-								onChange={onChange()}
-							/>
-						</FormGroup>
-					</div>
-				</div>
-				<div className="row full-width wrap">
-					<div className="min-half-width">
-						<FormGroup label="Account Type" className="min-half-width">
-							<AccountTypeSelect
-								intent="none"
-								activeItem={data.account_type}
-								onItemSelect={onChange('account_type')}
-							/>
-						</FormGroup>
-					</div>
-					<div className="min-half-width">
-						<FormGroup label="Authorised Buildings" className="min-half-width">
-							{data.account_type !== 'admin' ? (
-								<BuildingSelect
-									multiSelect
-									fill
-									activeItemList={data.buildings}
-									onItemSelect={onChange('buildings')}
-									multiSelectTagRemove={handleTagRemove}
-									clearButton={
-										data.buildings.length > 0 ? (
-											<Button icon="cross" minimal onClick={handleClear} />
-										) : undefined
-									}
-								/>
-							) : (
-								<Callout>Admin account type has access to all buildings</Callout>
-							)}
-						</FormGroup>
-					</div>
-				</div>
-				<div className="row justify-center" style={{ marginTop: '24px' }}>
-					<div className="min-quarter-width">
-						<Button fill intent="primary" outlined large onClick={handleAPICall}>
-							Create
-						</Button>
-					</div>
-				</div>
-			</Wrapper>
-		</div>
+		<UserForm
+			{...{
+				data,
+				setData,
+				handleAPICall,
+				handleClear,
+				handleTagRemove,
+			}}
+		/>
 	);
 };
 
--- frontend/components/Organisation/User/New/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin: 0px 64px;
-	align-items: stretch;
-	justify-content: space-between;
-`;
