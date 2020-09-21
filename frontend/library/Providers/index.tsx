2048494946ef337ddadb84b2025dc781b13006ef
Dipunj Gupta <dipunj.mnnit@gmail.com>
2020-09-22 03:54:18 +0530

    typescript changes, consumed added withAuthorisation, tested working

--- frontend/library/Providers/index.tsx
+++ frontend/library/Providers/index.tsx
@@ -1,12 +1,15 @@
 import SessionCtxProvider from '../Context/SessionContext';
 import ThemeCtxProvider from '../Context/ThemeContext';
 import BuildingCtxProvider from '../Context/BuildingContext';
+import WithAuthorisation from '../withAuth';
 
-const Providers = ({ children }: { children: JSX.Element[] }) => {
+const Providers = ({ children }: { children: JSX.Element[] | JSX.Element }) => {
 	return (
 		<SessionCtxProvider>
 			<BuildingCtxProvider>
-				<ThemeCtxProvider>{children}</ThemeCtxProvider>
+				<ThemeCtxProvider>
+					<WithAuthorisation>{children}</WithAuthorisation>
+				</ThemeCtxProvider>
 			</BuildingCtxProvider>
 		</SessionCtxProvider>
 	);
